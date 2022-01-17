92e8d0f62fb84f322e3213c5a019085022740e75
Johan Kladder <johankladder@gmail.com>
2022-01-17 15:50:02 +0100

    *

--- src/filesystem.py
+++ src/filesystem.py
@@ -5,7 +5,7 @@ class FileSystem:
 
     @staticmethod
     def get_dir_prefix() -> str:
-        return "documents/queue-item-"
+        return "/home/pi/PythonResourcePrinter/documents/queue-item-"
 
     @staticmethod
     def generate_file_path(queue_item_id: int) -> str:
