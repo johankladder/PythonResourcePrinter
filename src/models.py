8ac4689d258dd7f66fe306248636a26f1b59494b
Johan Kladder <johankladder@gmail.com>
2022-02-04 13:48:53 +0100

    + Added boolean fields

--- src/models.py
+++ src/models.py
@@ -4,16 +4,20 @@ class QueueItem:
     print_location: str
     print_location_mix: str
     n_mix: int = 0
+    print_items: True
+    print_mix: True
 
-    def __init__(self, queue_id: int, data: str, print_location: str, print_location_mix: str, n_mix: int):
+    def __init__(self, queue_id: int, data: str, print_location: str, print_location_mix: str, n_mix: int,
+                 print_mix: bool, print_items: bool):
         self.id = queue_id
         self.data = data
         self.print_location = print_location
         self.print_location_mix = print_location_mix
         self.n_mix = n_mix
+        self.print_mix = print_mix
+        self.print_items = print_items
 
 
 class Printer:
     def __init__(self, printer_id: str):
         self.printer_id = printer_id
-
--- src/network.py
+++ src/network.py
@@ -28,12 +28,13 @@ class Network:
 
 
 class PrinterQueueNetwork:
-
     base_64_key = "label_base64"
     id_key = "id"
     print_location_key = "print_location"
     print_location_mix_key = "print_location_mix"
     n_mix = "n_mix"
+    print_items_key = "print_items"
+    print_mix_key = "print_mix"
 
     def __init__(self, base_url: str, auth_token: str, network=Network()):
         self.base_url = base_url
@@ -45,12 +46,14 @@ class PrinterQueueNetwork:
 
         for item in self.network.get(self.base_url + self.get_authentication_end_fix(), handle=False).json()["data"]:
             items.append(QueueItem(
-                    queue_id=item[self.id_key],
-                    data=item[self.base_64_key],
-                    print_location=item.get(self.print_location_key, None),
-                    print_location_mix=item.get(self.print_location_mix_key, item.get(self.print_location_key, None)),
-                    n_mix=item.get(self.n_mix, 0)
-                )
+                queue_id=item[self.id_key],
+                data=item[self.base_64_key],
+                print_location=item.get(self.print_location_key, None),
+                print_location_mix=item.get(self.print_location_mix_key, item.get(self.print_location_key, None)),
+                n_mix=item.get(self.n_mix, 0),
+                print_items=item.get(self.print_items_key, True),
+                print_mix=item.get(self.print_mix_key, True)
+            )
             )
         return items
 
